30b63f481fb4bd2e9b982ca3e4e5ab7d74083609
Shu Ding <g@shud.in>
2025-10-20 12:06:35 +0200

    wip - visuals

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
       <ul className='lowercase text-right mobile:sticky top-6 sm:top-10 md:top-14 mb-6 mobile:mb-0 flex gap-2 justify-end mobile:block'>
         <Item href='/'>About</Item>
         <Item href='/thoughts'>Thoughts</Item>
-        {/* <Item href='/visuals'>Visuals</Item> */}
+        <Item href='/visuals'>Visuals</Item>
         <Item href='/projects'>Projects</Item>
         {/* <Item href='/guestbook'>Guestbook</Item> */}
       </ul>
