657530f4ce25513f816ad7a8f672f7f2c03cfb4e
Shu Ding <g@shud.in>
2025-10-20 17:46:28 +0200

    improve typography and gradient

--- app/thoughts/page.tsx
+++ app/thoughts/page.tsx
@@ -40,14 +40,14 @@ export default async function Page() {
           <li key={item.slug} className='font-medium'>
             <Link
               href={`/thoughts/${item.slug}`}
-              className='group flex gap-1 justify-between items-center'
+              className='group flex gap-1 -mx-2 px-2 justify-between items-center focus-visible:outline focus-visible:outline-rurikon-400 focus-visible:rounded-xs focus-visible:outline-dotted focus-visible:text-rurikon-600'
               draggable={false}
             >
-              <span className='block text-rurikon-500 group-hover:text-rurikon-700'>
+              <span className='block text-rurikon-500 group-hover:text-rurikon-700 group-focus-visible:text-rurikon-700'>
                 {item.title}
               </span>
-              <span className='text-sm dot-leaders flex-1 text-rurikon-100 font-normal group-hover:text-rurikon-500 transition-colors group-hover:transition-none leading-none' />
-              <time className='block text-rurikon-200 tabular-nums font-normal tracking-tighter group-hover:text-rurikon-500 transition-colors group-hover:transition-none self-start'>
+              <span className='text-sm dot-leaders flex-1 text-rurikon-100 font-normal group-hover:text-rurikon-500 group-focus-visible:text-rurikon-500 transition-colors group-hover:transition-none leading-none' />
+              <time className='block text-rurikon-200 tabular-nums font-normal tracking-tighter group-hover:text-rurikon-500 group-focus-visible:text-rurikon-500 transition-colors group-hover:transition-none self-start'>
                 {item.date}
               </time>
             </Link>
